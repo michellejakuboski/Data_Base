6a606bd90b80c7d114a76a6ec128d2d6c0d88998
Sofia Dos Santos <sofiaarantes1707@gmail.com>
2026-05-04 12:21:03 -0300

    Create tables for Produto, Venda, Compra, and Saldo
    This script creates tables for managing products, sales, purchases, and stock balance.

---
+++ BD2.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Produto
+(
+	ID_PRODUTO int primary key,
+	DESCRICAO varchar(80),
+	UN int,
+	PRECO float
+)
+
+CREATE TABLE Venda
+(
+	ID_VENDA int primary key,
+	DATA_VENDA datetime,
+	UN int,
+	PRECO float,
+	ID_PRODUTO int,
+	constraint ID_PRODUTO
+	foreign key (ID_PRODUTO)
+	references Produto(ID_PRODUTO)
+)
+
+CREATE TABLE Compra
+(
+	ID_COMPRA int primary key,
+	ID_PRODUTO int,
+	DATA_COMPRA datetime,
+	DESCRICAO varchar(80),
+	UN int,
+	PRECO float
+)
+
+CREATE TABLE Saldo
+(
+	ID_PRODUTO int primary key,
+	QTD int
+)

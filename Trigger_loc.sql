aa349112235efb89d2e62bdcfe3ff2fe24bc3715
Sofia Dos Santos <sofiaarantes1707@gmail.com>
2026-06-01 12:31:18 -0300

    Add trigger to set film status to 'RESERVADO'
    Create trigger to update film status after rental update.

---
+++ Trigger_loc.sql
@@ -0,0 +1,17 @@
+create trigger [dbo].[Alugado] em LOCACOES
+after update
+as
+begin 
+	select * from inserted
+	update dbo.FILME set ESTADO = 'RESERVADO' from dbo.FILME f inner join inserted i on f.COD_FILME = i.COD_FILME
+
+END
+GO 
+
+USE [db_250176];
+GO
+
+INSERT INTO [dbo].[LOCACOES] (COD_CLIENTE, COD_FILME, DATA_LOCACAO, DATA_EXPIRACAO, Data_Devolucao)
+VALUES (1, 10, GETDATE(), DATEADD(day, 3, GETDATE()), NULL);
+GO
+SELECT * FROM dbo.FILME WHERE COD_FILME = 10;
